9181e354ac63842c6e85a9155767aa60c0c4edd8
aaavieri <aaavieri@gmail.com>
2018-10-15 17:46:41 +0800

    add goods

---
+++ ddl&dml/20181015.1.add_goods_dictionary.sql
@@ -0,0 +1,25 @@
+INSERT INTO `t_dictionary`
+VALUES
+('t_mall_goods',
+'type_id',
+'ET',
+'儿童',
+0,
+CURRENT_TIMESTAMP,
+'system',
+CURRENT_TIMESTAMP,
+'system',
+1);
+
+INSERT INTO `t_dictionary`
+VALUES
+('t_mall_goods',
+'type_id',
+'XS',
+'新生儿',
+0,
+CURRENT_TIMESTAMP,
+'system',
+CURRENT_TIMESTAMP,
+'system',
+1);
